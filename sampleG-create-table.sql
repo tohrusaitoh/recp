a10090acb15a8b981788b3c8b1a9599f30a2b7e3
Tohru Saitoh <tsaitoh.net@gmail.com>
2021-11-01 12:28:01 +0900

    2021/11/01-maru

--- sampleG-create-table.sql
+++ sampleG-create-table.sql
@@ -1,12 +1,17 @@
--- 品目リスト --
+-- テーブルを作る
+--	最初に一度だけ実行する
+
+-- 品目リスト
+drop table ITEMLIST if exists ;
 create table ITEMLIST (
        id    integer		primary key ,
        name  varchar( 20 )	not null ,
-       price integer  	 	not null ,
+       price integer  	 	not null
 ) ;
 
--- 購入リスト --
+-- 購入リスト
+drop table BUYLIST if exists ;
 create table BUYLIST (
        id    integer		primary key , 
-       count integer		not null ,
+       count integer		not null
 ) ;
---
+++ sampleG-insert-buylist.sql
@@ -0,0 +1,19 @@
+-- 品目リスト
+-- create table ITEMLIST (
+--       id    integer		primary key ,
+--       name  varchar( 20 )	not null ,
+--       price integer  	not null ,
+-- ) ;
+
+insert into ITEMLIST ( id , name , price ) values( 1010 , 'みかん' ,         50 ) ;
+insert into ITEMLIST ( id , name , price ) values( 1020 , 'りんご' ,        100 ) ;
+insert into ITEMLIST ( id , name , price ) values( 1022 , 'パイナップル' , 1000 ) ;
+
+-- 購入リスト
+-- create table BUYLIST (
+--        id    integer		primary key , 
+--        count integer		not null ,
+-- ) ;
+insert into BYLIST( id , count ) values( 1010 , 5 ) ;
+insert into BYLIST( id , count ) values( 1020 , 2 ) ;
+insert into BYLIST( id , count ) values( 1022 , 1 ) ;
