6b600f823ee7d01e1c08eb408109a7b9f1e4b704
Tohru Saitoh <tsaitoh.net@gmail.com>
2021-11-01 12:31:22 +0900

    2021/11/01-maru

--- sampleG-insert-itemlist.sql
+++ sampleG-insert-itemlist.sql
@@ -8,12 +8,3 @@
 insert into ITEMLIST ( id , name , price ) values( 1010 , 'みかん' ,         50 ) ;
 insert into ITEMLIST ( id , name , price ) values( 1020 , 'りんご' ,        100 ) ;
 insert into ITEMLIST ( id , name , price ) values( 1022 , 'パイナップル' , 1000 ) ;
-
--- 購入リスト --
--- create table BUYLIST (
---        id    integer		primary key , 
---        count integer		not null ,
--- ) ;
-insert into BYLIST( id , count ) values( 1010 , 5 ) ;
-insert into BYLIST( id , count ) values( 1020 , 2 ) ;
-insert into BYLIST( id , count ) values( 1022 , 1 ) ;
